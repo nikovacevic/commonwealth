e385628803deacff73af1b17a6efb8e394455c00
Niko Kovacevic <nikovacevic@gmail.com>
2017-06-01 10:50:33 -0600

    Moved router into main and created a separate file in package main for routes only

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/nikovacevic/commonwealth/logger"
 )
-
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
-}
 
 func main() {
 	r := NewRouter()
 	http.ListenAndServe(":3000", r)
+}
+
+// NewRouter returns a gorilla/mux Router with registered routing based on
+// var routes (see routes.go)
+func NewRouter() *mux.Router {
+	r := mux.NewRouter()
+	// Register routes defined in routes
+	for _, route := range routes {
+		fn := logger.LogRequest(route.HandlerFunc)
+		r.HandleFunc(route.Path, fn).Methods(route.Method)
+	}
+	// Register route for static files
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	// Register special assets
+	r.HandleFunc("/favicon.ico", http.NotFound)
+	return r
 }
