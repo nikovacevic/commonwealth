019ad4d5b3ded4b2e6160c99400ba5dfbd3ec5ca
Niko Kovacevic <nikovacevic@gmail.com>
2017-06-28 09:37:04 -0600

    Added 404 status code header to 404 response writer

--- handlers/error.go
+++ handlers/error.go
@@ -6,5 +6,6 @@ import (
 
 // NotFound handles 404 errors
 func (hdl *Handler) NotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
 	hdl.Render(w, "404.gohtml", nil)
 }
