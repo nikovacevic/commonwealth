65f743f0f5910a871eb91e0fff2cc7cb9b9c3633
Niko Kovacevic <nikovacevic@gmail.com>
2017-07-31 15:12:47 -0400

    Started a TODO list

--- psql/create_users.sql
+++ psql/create_users.sql
@@ -1,13 +1,10 @@
 CREATE TABLE users (
-  id integer NOT NULL,
+  id SERIAL PRIMARY KEY,
   first_name text NOT NULL,
   last_name text NOT NULL,
-  email text NOT NULL,
-  phone text NOT NULL,
+  email text UNIQUE NOT NULL,
+  phone text UNIQUE NOT NULL,
   organization text,
   password_hash text NOT NULL,
   create_date timestamp without time zone DEFAULT now()
 );
-ALTER TABLE ONLY users ADD CONSTRAINT email UNIQUE (email);
-ALTER TABLE ONLY users ADD CONSTRAINT phone UNIQUE (phone);
-ALTER TABLE ONLY users ADD CONSTRAINT users_pkey PRIMARY KEY (id);
