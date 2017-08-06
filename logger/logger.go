688727db2e9c7a256eb2d98aea6afb6ab839435b
Niko Kovacevic <nikovacevic@gmail.com>
2017-08-06 17:50:46 -0600

    Updated logger to maniuplate http.Handlers instead of HandlerFuncs, making the logging possibilities more flexible.

--- logger/logger.go
+++ logger/logger.go
@@ -7,29 +7,29 @@ import (
 )
 
 // LogRequest wraps a HandlerFunc with logging to stdout
-func LogRequest(hf http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		hf(w, r)
+		next.ServeHTTP(w, r)
 		log.Printf(
 			"%-6s\t%-30s\t%s",
 			r.Method,
 			r.RequestURI,
 			time.Since(start),
 		)
-	}
+	})
 }
 
 // LogError wraps an erroneous request's HandlerFunc with logging to stdout
-func LogError(code string, hf http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func LogError(code string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		hf(w, r)
+		next.ServeHTTP(w, r)
 		log.Printf(
 			"%-6s\t%-30s\t%s",
 			code,
 			r.RequestURI,
 			time.Since(start),
 		)
-	}
+	})
 }
